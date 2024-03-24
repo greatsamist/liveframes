7bcfe5b514176812a1f93c3c8341a1e3c8bc3d2c
greatsamist <ifegbenga@gmail.com>
2024-03-24 09:38:14 +0100

    test frames

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -1,31 +1,68 @@
+import { DEFAULT_DEBUGGER_HUB_URL } from "@/lib/debug";
+import { currentURL } from "@/lib/utils";
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameInput,
+  FrameReducer,
+  NextServerPageProps,
+  getFrameMessage,
+  getPreviousFrame,
+  useFramesReducer,
+} from "frames.js/next/server";
 import Link from "next/link";
 
-import type { Metadata } from "next";
-import { fetchMetadata } from "frames.js/next";
-import { currentURL, vercelURL } from "@/lib/utils";
-import { createDebugUrl } from "@/lib/debug";
+type State = {
+  active: string;
+  total_button_presses: number;
+};
 
-export async function generateMetadata(): Promise<Metadata> {
+const initialState = { active: "1", total_button_presses: 0 };
+
+const reducer: FrameReducer<State> = (state, action) => {
   return {
-    title: "New api example",
-    description: "This is a new api example",
-    other: {
-      ...(await fetchMetadata(
-        new URL("/mint-button/frames", vercelURL() || "http://localhost:3000")
-      )),
-    },
+    total_button_presses: state.total_button_presses + 1,
+    active: action.postBody?.untrustedData.buttonIndex
+      ? String(action.postBody?.untrustedData.buttonIndex)
+      : "1",
   };
-}
+};
 
-export default async function Home() {
-  const url = currentURL("/mint-button");
+// This is a react server component only
+export default async function Mint({ searchParams }: NextServerPageProps) {
+  const url = currentURL("/");
+  const previousFrame = getPreviousFrame<State>(searchParams);
+
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+  });
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+  const [state, dispatch] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
 
   return (
-    <div>
-      New api mint button example.{" "}
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>
+    <div className="p-4">
+      <FrameContainer
+        postUrl="/frames"
+        pathname="/mint"
+        state={state}
+        previousFrame={previousFrame}
+      >
+        <FrameImage src={searchParams?.image as string} />
+
+        {/* <FrameInput text="put some text here" /> */}
+
+        <FrameButton action="mint" target={searchParams?.link as string}>
+          Mint
+        </FrameButton>
+      </FrameContainer>
     </div>
   );
 }
